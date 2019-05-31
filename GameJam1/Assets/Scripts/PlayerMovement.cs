596d87983ad0940049b82ae6e5e82b38e93b3baa
Unknown <frederik_buur@hotmail.com>
2019-05-31 22:41:27 +0200

    Dunno Lars Stjæler

---
+++ GameJam1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerNum = 1;
+    public int speed = 10;
+
+    private float moveDir = 0;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDir = (playerNum == 1 ? Input.GetAxisRaw("Horizontal1") : Input.GetAxisRaw("Horizontal2"));
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(moveDir * speed, rb.velocity.y);
+
+        rb.AddForce(movement);
+    }
+}
